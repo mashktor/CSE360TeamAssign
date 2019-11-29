58f3f6d61cdbd30e7381e0a893b70f9e30d74b0a
Miras Ashktorab <mashktor@asu.edu>
2019-11-29 13:56:57 -0700

    Finished Navigation for the menubar, still need to implement deactive protocol in so that menubuttons are deactivated until a data set has been created.

--- src/teamAssign/DisplayData.java
+++ src/teamAssign/DisplayData.java
@@ -3,14 +3,12 @@ package teamAssign;
 import com.sun.deploy.uitoolkit.impl.fx.ui.FXMessageDialog;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
@@ -22,6 +20,108 @@ import java.util.Optional;
 
 public class DisplayData {
 
+    @FXML
+    MenuBar myMenuBar;
+
+    @FXML
+    protected void errorLogMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("ErrorLog.fxml"));
+        Scene parentScene = new Scene(parent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(parentScene);
+        window.show();
+    };
+
+    @FXML
+    protected void displayGraphMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("DisplayGraph.fxml"));
+        Scene parentScene = new Scene(parent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(parentScene);
+        window.show();
+    };
+
+    @FXML
+    protected void displayDistMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("DisplayDist.fxml"));
+        Scene parentScene = new Scene(parent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(parentScene);
+        window.show();
+    };
+
+    @FXML
+    protected void displayDataMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("DisplayData.fxml"));
+        Scene parentScene = new Scene(parent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(parentScene);
+        window.show();
+    };
+
+    @FXML
+    protected void onLoadFileMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent newDataSetParent = FXMLLoader.load(getClass().getResource("NewDataSet.fxml"));
+        Scene newDataSetScene = new Scene(newDataSetParent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(newDataSetScene);
+        window.show();
+    };
+
+    @FXML
+    public void deleteSingleMenuBtn(javafx.event.ActionEvent actionEvent) throws  IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DeleteSingle.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+
+    }
+
+    @FXML
+    protected void AddSingleMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddSingle.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+    };
+
+    @FXML
+    protected void OnSetAppendDataMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AppendData.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+    };
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void newDataSetBtnClk(javafx.event.ActionEvent actionEvent) throws IOException {
         Parent newDataSetParent = FXMLLoader.load(getClass().getResource("NewDataSet.fxml"));
         Scene newDataSetScene = new Scene(newDataSetParent);
@@ -75,7 +175,6 @@ public class DisplayData {
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setScene(scene);
         stage.showAndWait();
-
     }
 
     public void deleteSingleBtn(javafx.event.ActionEvent actionEvent) throws  IOException {
--- src/teamAssign/DisplayGraph.java
+++ src/teamAssign/DisplayGraph.java
@@ -1,14 +1,120 @@
 package teamAssign;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class DisplayGraph {
+
+    @FXML
+    MenuBar myMenuBar;
+
+    @FXML
+    protected void errorLogMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("ErrorLog.fxml"));
+        Scene parentScene = new Scene(parent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(parentScene);
+        window.show();
+    };
+
+    @FXML
+    protected void displayGraphMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("DisplayGraph.fxml"));
+        Scene parentScene = new Scene(parent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(parentScene);
+        window.show();
+    };
+
+    @FXML
+    protected void displayDistMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("DisplayDist.fxml"));
+        Scene parentScene = new Scene(parent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(parentScene);
+        window.show();
+    };
+
+    @FXML
+    protected void displayDataMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("DisplayData.fxml"));
+        Scene parentScene = new Scene(parent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(parentScene);
+        window.show();
+    };
+
+    @FXML
+    protected void onLoadFileMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent newDataSetParent = FXMLLoader.load(getClass().getResource("NewDataSet.fxml"));
+        Scene newDataSetScene = new Scene(newDataSetParent);
+
+        // This line gets the stage information
+        Stage window  = (Stage) myMenuBar.getScene().getWindow();
+
+        window.setScene(newDataSetScene);
+        window.show();
+    };
+
+    @FXML
+    public void deleteSingleMenuBtn(javafx.event.ActionEvent actionEvent) throws  IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DeleteSingle.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+
+    }
+
+    @FXML
+    protected void AddSingleMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddSingle.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+    };
+
+    @FXML
+    protected void OnSetAppendDataMenuBtn(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AppendData.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+    };
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void displayDataBtn(javafx.event.ActionEvent actionEvent) throws IOException {
         Parent newDataSetParent = FXMLLoader.load(getClass().getResource("DisplayData.fxml"));
