c12d0db980125f0bfa326a9730481593b74d717d
Miras Ashktorab <mashktor@asu.edu>
2019-12-03 20:36:00 -0700

    Changes made by edward to appenddata and deletesingle

--- src/teamAssign/DeleteSingle.java
+++ src/teamAssign/DeleteSingle.java
@@ -1,6 +1,33 @@
 package teamAssign;
 
-public class DeleteSingle
-{
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+import static teamAssign.Main.dataSet;
+
+public class DeleteSingle {
+
+    @FXML
+    private TextField input;
+
+
+    public void populateTable(ActionEvent event) throws IOException
+    {
+        //gets the user input from the TextField and parses it to a float
+        float grade = Float.parseFloat(input.getText());
+        //loop searches for all the grades in the ArrayList
+        for(int i = 0; i < dataSet.size(); i++)
+        {
+            if(grade == dataSet.get(i))
+            {
+
+            }
+        }
+    }
 }
